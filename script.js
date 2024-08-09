22ca01789bce194a6d0c8dd9d86f315d8c150919
levbitz <maxinosamx@gmail.com>
2024-08-10 00:41:16 +0300

    fdkjdk

--- script.js
+++ script.js
@@ -3,6 +3,6 @@
 
 
  // Function to add the blur class to the body after 5 seconds
- setTimeout(function() {
-  document.body.classList.add('blur');
-}, 5000);
+//  setTimeout(function() {
+//   document.body.classList.add('blur');
+// }, 5000);
